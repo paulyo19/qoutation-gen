0ef7f391721c6cc0fa97a97eb2df3314f17cad08
Paul C <catkillercleary@gmail.com>
2018-12-13 00:09:30 +0000

    added arrays for my qoute generation

---
+++ script.js
@@ -0,0 +1,12 @@
+var start = ['The young child', 'The old man', 'An entire family', 'The tall bloke', 'A happy kid', 'The dumb blond', 'A smart lady', 'The big dog', 'The tiny dwarf', 'A famous celebrity', 'The crazy scientist', 'The moody teenager', 'The handy man', 'The clumsy clown'];
+
+
+var middle = ['paints a picture', 'with a huge smile', 'took a photo', 'was running late', 'started to record', 'took a trip', 'was really lost', 'with no clue', 'with giant hands', 'with sharp thinking', 'with no clothes', 'with bad breath', 'with bad tattoos', 'with bad timing', 'with weird hair'];
+
+
+var end = ['falls into bush', 'slipped on ice', 'got sea sick', 'got a present', 'lost their wallet', 'never seen again', 'had sun stroke', 'got a lollipop', 'went really crazy', 'washed their car', 'went for shopping', 'drove for hours', 'lost their car', 'needed an ambulance'];
+
+var where = ['on an island', 'in the jungle', 'on the moutains', 'in the dessert', 'up in space', 'under the ocean', 'in their room', 'in a small house', 'in the city', 'in the lakes', 'on the aeroplane', 'in the sky', 'on the moon', 'at the beach'];
+
+var when = ['back in 1960', 'at a wedding', 'at a funeral', 'in the future', 'in world war1', 'in stone age', 'in the 70s', 'at a party', 'in the ice age', 'at world cup', 'with the dinosours', 'with jesus christ', 'with bruce lee', 'at new years'];
+
