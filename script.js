bffc3c3c1318e83d1be168c4b14c2dae27eedacc
Paul C <catkillercleary@gmail.com>
2018-12-13 22:04:36 +0000

    used bootstrap to change the layout of the page and added functions for the javascript code to make it more simpler

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
-var start = ['The young child', 'The old man', 'An entire family', 'The tall bloke', 'A happy kid', 'The dumb blond', 'A smart lady', 'The big dog', 'The tiny dwarf', 'A famous celebrity', 'The crazy scientist', 'The moody teenager', 'The handy man', 'The clumsy clown'];
+var type = document.querySelector('#type');
+var qty = document.querySelector('#qty');
+var generateBtn = document.querySelector('#generate');
+
+var quotes = {
+    humans: {
+        start: ['The young child', 'The old man', 'An entire family', 'The tall bloke', 'A happy kid', 'The dumb blond', 'A smart lady', 'The big dog', 'The tiny dwarf', 'A famous celebrity', 'The crazy scientist', 'The moody teenager', 'The handy man', 'The clumsy clown'],
+        middle: ['paints a picture', 'with a huge smile', 'took a photo', 'was running late', 'started to record', 'took a trip', 'was really lost', 'with no clue', 'with giant hands', 'with sharp thinking', 'with no clothes', 'with bad breath', 'with bad tattoos', 'with bad timing', 'with weird hair'],
+        end: ['falls into bush', 'slipped on ice', 'got sea sick', 'got a present', 'lost their wallet', 'never seen again', 'had sun stroke', 'got a lollipop', 'went really crazy', 'washed their car', 'went for shopping', 'drove for hours', 'lost their car', 'needed an ambulance'],
+        where: ['on an island', 'in the jungle', 'on the moutains', 'in the dessert', 'up in space', 'under the ocean', 'in their room', 'in a small house', 'in the city', 'in the lakes', 'on the aeroplane', 'in the sky', 'on the moon', 'at the beach'],
+        when: ['back in 1960', 'at a wedding', 'at a funeral', 'in the future', 'in world war1', 'in stone age', 'in the 70s', 'at a party', 'in the ice age', 'at world cup', 'with the dinosours', 'with jesus christ', 'with bruce lee', 'at new years']
+    },
+    animals: {
+        start: ['The young child', 'The old man', 'An entire family', 'The tall bloke', 'A happy kid', 'The dumb blond', 'A smart lady', 'The big dog', 'The tiny dwarf', 'A famous celebrity', 'The crazy scientist', 'The moody teenager', 'The handy man', 'The clumsy clown'],
+        middle: ['paints a picture', 'with a huge smile', 'took a photo', 'was running late', 'started to record', 'took a trip', 'was really lost', 'with no clue', 'with giant hands', 'with sharp thinking', 'with no clothes', 'with bad breath', 'with bad tattoos', 'with bad timing', 'with weird hair'],
+        end: ['falls into bush', 'slipped on ice', 'got sea sick', 'got a present', 'lost their wallet', 'never seen again', 'had sun stroke', 'got a lollipop', 'went really crazy', 'washed their car', 'went for shopping', 'drove for hours', 'lost their car', 'needed an ambulance'],
+        where: ['on an island', 'in the jungle', 'on the moutains', 'in the dessert', 'up in space', 'under the ocean', 'in their room', 'in a small house', 'in the city', 'in the lakes', 'on the aeroplane', 'in the sky', 'on the moon', 'at the beach'],
+        when: ['back in 1960', 'at a wedding', 'at a funeral', 'in the future', 'in world war1', 'in stone age', 'in the 70s', 'at a party', 'in the ice age', 'at world cup', 'with the dinosours', 'with jesus christ', 'with bruce lee', 'at new years']
+    }
+}
 
 
-var middle = ['paints a picture', 'with a huge smile', 'took a photo', 'was running late', 'started to record', 'took a trip', 'was really lost', 'with no clue', 'with giant hands', 'with sharp thinking', 'with no clothes', 'with bad breath', 'with bad tattoos', 'with bad timing', 'with weird hair'];
+
+function generateQuote(type) {
 
 
-var end = ['falls into bush', 'slipped on ice', 'got sea sick', 'got a present', 'lost their wallet', 'never seen again', 'had sun stroke', 'got a lollipop', 'went really crazy', 'washed their car', 'went for shopping', 'drove for hours', 'lost their car', 'needed an ambulance'];
+    var part1 = quotes[type].start[Math.floor(Math.random() * quotes[type].start.length)];
+    var part2 = quotes[type].middle[Math.floor(Math.random() * quotes[type].middle.length)];
+    var part3 = quotes[type].end[Math.floor(Math.random() * quotes[type].end.length)];
+    var part4 = quotes[type].where[Math.floor(Math.random() * quotes[type].where.length)];
+    var part5 = quotes[type].when[Math.floor(Math.random() * quotes[type].when.length)];
 
-var where = ['on an island', 'in the jungle', 'on the moutains', 'in the dessert', 'up in space', 'under the ocean', 'in their room', 'in a small house', 'in the city', 'in the lakes', 'on the aeroplane', 'in the sky', 'on the moon', 'at the beach'];
+    return part1 + " " + part2 + " " + part3 + " " + part4 + " " + part5;
+}
 
-var when = ['back in 1960', 'at a wedding', 'at a funeral', 'in the future', 'in world war1', 'in stone age', 'in the 70s', 'at a party', 'in the ice age', 'at world cup', 'with the dinosours', 'with jesus christ', 'with bruce lee', 'at new years'];
 
+generateBtn.addEventListener('click', function() {
+     
+
+    for(var i = 0; i < parseInt(qty.value); i++) {
+        console.log('quote:', generateQuote(type.value))        
+    }
+
+    console.log('ended loop...')
+})
