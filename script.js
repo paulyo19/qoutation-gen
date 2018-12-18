e58233096f05666edd5e0ca79a5dd6847d5b89f7
Paul C <catkillercleary@gmail.com>
2018-12-18 22:47:53 +0000

    getting rid of code not needed

--- script.js
+++ script.js
@@ -2,29 +2,6 @@ var type = document.querySelector('#type');
 var qty = document.querySelector('#qty');
 var generateBtn = document.querySelector('#generate');
 
-var numQuotes = 1;
-var mixedNum = 5;
-
-
-function setNumQuotesTo1() {
-    numQuotes = 1;
-}
-function setNumQuotesTo2() {
-    numQuotes = 2;
-}
-function setNumQuotesTo3() {
-    numQuotes = 3;
-}
-function setNumQuotesTo4() {
-    numQuotes = 4;
-}
-function setNumQuotesTo5() {
-    numQuotes = 5;
-}
-
-
-
-
 
 function generateQuote(type) {
 
@@ -38,15 +15,14 @@ function generateQuote(type) {
 }
 
 
-generateBtn.addEventListener('click', function() {
-    for(var i = 0; i < parseInt(qty.value); i++) {
+generateBtn.addEventListener('click', function () {
+    for (var i = 0; i < parseInt(qty.value); i++) {
         document.querySelector('#quote').textContent = generateQuote(type.value);
-//    console.log('quote:', generateQuote(type.value))   
-            
+        //    console.log('quote:', generateQuote(type.value))   
+
     }
     console.log('ended loop...');
 });
-
 
 
 
@@ -59,10 +35,13 @@ var quotes = {
         when: ['back in 1960', 'at a wedding', 'at a funeral', 'in the future', 'in world war1', 'in stone age', 'in the 70s', 'at a party', 'in the ice age', 'at world cup', 'with the dinosours', 'with jesus christ', 'with bruce lee', 'at new years']
     },
     animals: {
-        start: ['The young child', 'The old man', 'An entire family', 'The tall bloke', 'A happy kid', 'The dumb blond', 'A smart lady', 'The big dog', 'The tiny dwarf', 'A famous celebrity', 'The crazy scientist', 'The moody teenager', 'The handy man', 'The clumsy clown'],
-        middle: ['paints a picture', 'with a huge smile', 'took a photo', 'was running late', 'started to record', 'took a trip', 'was really lost', 'with no clue', 'with giant hands', 'with sharp thinking', 'with no clothes', 'with bad breath', 'with bad tattoos', 'with bad timing', 'with weird hair'],
-        end: ['falls into bush', 'slipped on ice', 'got sea sick', 'got a present', 'lost their wallet', 'never seen again', 'had sun stroke', 'got a lollipop', 'went really crazy', 'washed their car', 'went for shopping', 'drove for hours', 'lost their car', 'needed an ambulance'],
+        start: ['The small puppy', 'The old cat', 'A huge elephant', 'A blind dog', 'A talking parrot', 'The dumb ape', 'A smart chimp', 'The big dog', 'The tiny mouse', 'A tired horse', 'The fast cheetah', 'The hungry lion', 'A flock of geese', 'The flying bird'],
+        middle: ['had no choice', 'with a huge smile', 'starts to', 'ends up', 'with no idea', 'took the', 'was really lost', 'with no clue', 'with a', 'with sharp thinking', 'with no direction', 'with bad judgement', 'cant believe', 'with bad timing', 'stops to'],
+        end: ['started to talk', 'had a collar', 'got sea sick', 'got a present', 'lost their owner', 'never seen again', 'had to much to eat', 'got a scared', 'went really crazy', 'washed themselves', 'went for food', 'lost for hours', 'needed help', 'needed a vet'],
         where: ['on an island', 'in the jungle', 'on the moutains', 'in the dessert', 'up in space', 'under the ocean', 'in their room', 'in a small house', 'in the city', 'in the lakes', 'on the aeroplane', 'in the sky', 'on the moon', 'at the beach'],
         when: ['back in 1960', 'at a wedding', 'at a funeral', 'in the future', 'in world war1', 'in stone age', 'in the 70s', 'at a party', 'in the ice age', 'at world cup', 'with the dinosours', 'with jesus christ', 'with bruce lee', 'at new years']
     }
 }
+
+
+//    console.log('quote:', generateQuote(type.value)) 
