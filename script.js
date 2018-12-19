2f33d958d3a6823272323b8abc332c411edaf42c
Paul C <catkillercleary@gmail.com>
2018-12-19 22:21:09 +0000

    changed the quotes for humans and animals so none of them where the same

--- script.js
+++ script.js
@@ -31,15 +31,15 @@ var quotes = {
         start: ['The young child', 'The old man', 'An entire family', 'The tall bloke', 'A happy kid', 'The dumb blond', 'A smart lady', 'The idiot man', 'The tiny dwarf', 'A famous celebrity', 'The crazy scientist', 'The moody teenager', 'The handy man', 'The clumsy clown'],
         middle: ['paints a picture', 'with a huge smile', 'took a photo', 'was running late', 'started to record', 'took a trip', 'was really lost', 'with no clue', 'with giant hands', 'with sharp thinking', 'with no clothes', 'with bad breath', 'with bad tattoos', 'with bad timing', 'with weird hair'],
         end: ['falls into bush', 'slipped on ice', 'got sea sick', 'got a present', 'lost their wallet', 'never seen again', 'had sun stroke', 'got a lollipop', 'went really crazy', 'washed their car', 'went for shopping', 'drove for hours', 'lost their car', 'needed an ambulance'],
-        where: ['on an island', 'in the jungle', 'on the moutains', 'in the dessert', 'up in space', 'under the ocean', 'in their room', 'in a small house', 'in the city', 'in the lakes', 'on the aeroplane', 'in the sky', 'on the moon', 'at the beach'],
-        when: ['back in 1960', 'at a wedding', 'at a funeral', 'in the future', 'in world war1', 'in stone age', 'in the 70s', 'at a party', 'in the ice age', 'at world cup', 'with the dinosours', 'with jesus christ', 'with bruce lee', 'at new years']
+        where: ['near the center', 'in the city', 'on the road', 'in their job', 'up in space', 'at their house', 'in their room', 'in a small house', 'in the suburbs', 'at the lakes', 'on the aeroplane', 'in the sky', 'on the moon', 'at the beach'],
+        when: ['back in 1960', 'at a wedding', 'at a funeral', 'at 2pac concert', 'in world warI', 'in world warII', 'in the 70s', 'at a party', 'at the last supper', 'at world cup', 'with the dinosours', 'with jesus christ', 'with bruce lee', 'on the titanic']
     },
     animals: {
         start: ['The small puppy', 'The old cat', 'A huge elephant', 'A blind dog', 'A talking parrot', 'The dumb ape', 'A smart chimp', 'The big dog', 'The tiny mouse', 'A tired horse', 'The fast cheetah', 'The hungry lion', 'A flock of geese', 'The flying bird'],
         middle: ['had no choice', 'with a huge smile', 'starts to', 'ends up', 'with no idea', 'took the', 'was really lost', 'with no clue', 'with a', 'with sharp thinking', 'with no direction', 'with bad judgement', 'cant believe', 'with bad timing', 'stops to'],
         end: ['started to talk', 'had a collar', 'got sea sick', 'got a present', 'lost their owner', 'never seen again', 'had to much to eat', 'got a scared', 'went really crazy', 'washed themselves', 'went for food', 'lost for hours', 'needed help', 'needed a vet'],
-        where: ['on an island', 'in the jungle', 'on the moutains', 'in the dessert', 'up in space', 'under the ocean', 'in their room', 'in a small house', 'in the city', 'in the lakes', 'on the aeroplane', 'in the sky', 'on the moon', 'at the beach'],
-        when: ['back in 1960', 'at a wedding', 'at a funeral', 'in the future', 'in world war1', 'in stone age', 'in the 70s', 'at a party', 'in the ice age', 'at world cup', 'with the dinosours', 'with jesus christ', 'with bruce lee', 'at new years']
+        where: ['on an island', 'in the jungle', 'on the moutains', 'in the dessert', 'in the zoo', 'under the ocean', 'in their hut', 'in a small house', 'in the park', 'in the amazon', 'in the backyard', 'in the forest', 'in the vet', 'at the safari'],
+        when: ['in ancient times', 'in 1978', 'back in 1800', 'in the future', 'with the romans', 'in stone age', 'in the 40s', 'with the cowboys', 'in the ice age', 'at world cup', 'with the dinosours', 'with neil armstrong', 'at woodstock', 'at new years']
     }
 }
 
